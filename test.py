bfda1f28bd088fcf2f3fe87f664df6f772ac7702
Dievas100 <marius.kavaliauskas.mail@gmail.com>
2025-05-29 19:52:32 +0300

    final 29

--- test.py
+++ test.py
@@ -1,2 +1,28 @@
-print("hi")
-test
+message = "hello" #string
+print(message)
+x = 4 #integer
+age = 18 #integer
+height = 1.80 #float
+is_married = False #boolean
+
+is_old_enough = age >= 25
+
+age = input("Prasome atstoti nuo manes")
+
+if is_old_enough:
+    print("yes")
+
+print(is_old_enough)
+
+if age > 12:
+    print("You are an adult.")
+
+elif age>=20:
+    print("You are in your twenties.")
+
+elif age>=17:
+    print("You are in your forties.")
+
+age = int(age_str)
+
+
